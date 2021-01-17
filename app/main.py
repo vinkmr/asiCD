a92574bab7b1ac91232028d5ef0676bfb6a20b32
Joseph Sebastian Valiyaveettil <josephsv96@gmail.com>
2021-01-17 17:08:21 +0100

    updaed app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from pathlib import Path
+
+from numpy.testing._private.utils import print_assert_equal
 
 app = Flask(__name__)
 
@@ -8,19 +11,18 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("home.html", img_id="images/cloud.jpg")
+    return render_template("home.html", img_id="images/cloud_1.jpg")
 
 
 @app.route('/home', methods=['POST'])
 def process_post():
     img_dir = request.form.get("img_dir")
 
-    print(img_dir)
-
+    print(f"Img Dir: {img_dir}")
     new_img_id = request.form.get("img_file")
     new_img_id = img_dir + "/" + new_img_id + '.jpg'
 
-    print(new_img_id)
+    print(f"Img path: {new_img_id}")
 
     return render_template("home.html", img_id=new_img_id)
 
